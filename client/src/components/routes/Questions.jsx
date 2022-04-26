d63d7033d4ba8df8e122dedaea8bc7a443736290
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-04-26 15:19:11 +0300

    changes client

---
+++ client/src/components/routes/Questions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Questions() {
+    return (
+        <div>
+            <h1>This is the Questions page</h1>
+        </div>
+    )
+}
+
+export default Questions
