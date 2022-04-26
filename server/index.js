a2316cb881a5f4336404e55bbbcabde1a5310c4b
אירינה קטלקין <ikatalkin@nitc.co.il>
2022-04-27 01:48:32 +0300

    added translation's files

--- server/index.js
+++ server/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
 
+mongoose
+  .connect(process.env.DB_CONNECTION)
+  .then(() => {
+    console.log("connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.log("Could not connect to Mongodb", err);
+  });
+  
 const port = process.env.PORT || 3500;
 app.listen(port, () => console.log(`Listening on port ${port}`));
